a35ecb621b3fae631c6cf7d65705ed3901319a05
Jesse Koldewijn <jesse@iungo.nl>
2023-05-22 16:07:34 +0200

    fix: added preserve of selected theme in LS

--- src/layouts/navbar/themeToggle.tsx
+++ src/layouts/navbar/themeToggle.tsx
@@ -1,6 +1,6 @@
 export const prerender = true;
 
-import { createSignal } from "solid-js";
+import { createEffect, createSignal } from "solid-js";
 
 import { FaRegularMoon } from "solid-icons/fa";
 import { WiDaySunny } from "solid-icons/wi";
@@ -9,21 +9,23 @@ const ThemeToggle = () => {
   type theme = "light" | "dark";
   const [currentTheme, setCurrentTheme] = createSignal<theme>("dark");
 
-  if (typeof window !== "undefined") {
+  createEffect(() => {
     if (localStorage.getItem("theme")) {
       const currentTheme = localStorage.getItem("theme") as theme;
       setCurrentTheme(currentTheme);
     }
-  }
+  });
 
   const setThemeToggle = () => {
     const htmlRoot = document.querySelector("html");
     if (currentTheme() == "light") {
       setCurrentTheme("dark");
       htmlRoot.dataset.theme = "dark";
+      localStorage.setItem("theme", "dark");
     } else {
       setCurrentTheme("light");
       htmlRoot.dataset.theme = "light";
+      localStorage.setItem("theme", "light");
     }
   };
 
