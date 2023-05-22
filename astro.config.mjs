c79c27bc5a88b13be0a580152e05f9075da2b9e5
Jesse Koldewijn <jesse@iungo.nl>
2023-05-22 16:34:00 +0200

    update: added sitemap + robots + seotags flow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,29 @@ import { defineConfig } from "astro/config";
 // Integrations
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-
 import vercel from "@astrojs/vercel/serverless";
+import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://flash-jkinsight.vercel.app",
   output: "hybrid",
   experimental: {
-    hybridOutput: true
+    hybridOutput: true,
   },
   adapter: vercel(),
-  integrations: [tailwind(), solidJs()],
+  integrations: [
+    tailwind(),
+    solidJs(),
+    compress({
+      path: "./.vercel/output/static",
+    }),
+    sitemap(),
+    robotsTxt({
+      sitemap: true,
+    }),
+  ],
 });
